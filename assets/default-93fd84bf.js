a9620fefd9a4d838a08898da9c000e4dc8237881
singni <singni@outlook.com>
2023-04-01 01:58:53 +0800

    reinit

---
+++ assets/default-93fd84bf.js
@@ -0,0 +1,1 @@
+import a from"./index-b48fce27.js";import c from"./index-bb3a73c6.js";import{_ as s,c as _,b as e,d as m,F as p,o as d,r as o}from"./index-3befb68a.js";const l={components:{Header:a,Footer:c}};function f(i,u,F,$,v,x){const t=o("Header"),r=o("router-view"),n=o("Footer");return d(),_(p,null,[e(t),m("main",null,[e(r)]),e(n)],64)}const w=s(l,[["render",f]]);export{w as default};
---
+++ assets/login-952bed41.js
@@ -0,0 +1,1 @@
+import{_ as o,e as r,o as n,r as t}from"./index-3befb68a.js";const c={};function s(_,a){const e=t("router-view");return n(),r(e)}const i=o(c,[["render",s]]);export{i as default};
