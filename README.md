d45e02598429a00e9b263436bff66668a754b9f3
singni <singni@outlook.com>
2023-04-06 11:01:11 +0800

    'readfix'

--- README.md
+++
@@ -1,5 +0,0 @@
-# yozora-anime
-夜空动漫 power by 真夜真影 Email: 2190900595@qq.com
-
-webside:
-https://psysber.github.io/yozora-anime/#/
--- assets/default-ec643496.js
+++
@@ -1,1 +0,0 @@
-import a from"./index-569c1472.js";import c from"./index-7213fa37.js";import{_ as s,c as _,b as e,a as p,o as m,r as o}from"./index-b129c7fc.js";const d={components:{Header:a,Footer:c}};function i(l,f,u,v,$,x){const t=o("Header"),r=o("router-view"),n=o("Footer");return m(),_("div",null,[e(t),p("main",null,[e(r)]),e(n)])}const k=s(d,[["render",i]]);export{k as default};
